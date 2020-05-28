488beb33a6eeb6c848ee3fce1ec24aaae5ac607a
Lucas <rezendao.contato@gmail.com>
2020-05-28 16:37:38 -0300

    CAMELCASE + Atalhos

---
+++ PrimeiroProjeto/src/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,16 @@
+package primeiroprojeto; // Aula 04
+
+public class PrimeiroProjeto {
+    /*  
+        Formas de escrita de cada tipo CAMELCASE:
+        MinhaClasse - AlunosCursoEmVideo
+        meuAtributo - nomeAluno
+        minhaVariável - mediaPrimeiroBimestre
+        meuMétodo - lancarNotaAluno
+        meu_pacote - alunoscursoemvideo
+        MINHA_CONSTANTE - VALOR_DE_PI
+    */
+    public static void main(String[] args) { //Se você escrever psvm + TAB ele completa automaticamente.
+        System.out.println("Olá, mundo!"); // Se você escrever sout + TAB ele completa automaticamente.
+    }
+}
